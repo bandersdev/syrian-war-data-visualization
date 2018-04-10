6de0f3d1aeba5602ab0aa0f681c00281e89c0d0d
Bryan Anders <bryaneanders@gmail.com>
2018-04-09 19:29:25 -0700

    Got a GET request going for total fatalities and a basic string display. Need to figure out handlebar file duplication requirements.

--- server.js
+++ server.js
@@ -10,12 +10,47 @@ var hbs = exphbs.create({
             ]
           });
 
+var totalfatalities = require('./totalfatalities');
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.locals.layout = 'index';
 
 app.get('/', function(request, response) {
   var context = { title: 'Syrian Civil War Visualization' };
   response.render('index', context);
 });
 
+app.get('/totalfatalities', function(request, response) {
+  totalfatalities.getTotalFatalities(totalFatalatiesOptions, function(statusCode, result) {
+      var resultstring = JSON.stringify(result);
+      var context = {
+        layout: 'totalfatalities',
+        title: 'Syrian Civil War Visualization',
+        data: resultstring
+      };
+
+      // I could work with the result html/json here.  I could also just return it
+      console.log("onResult: (" + statusCode + ")" + resultstring);
+      response.statusCode = statusCode;
+      response.render('totalfatalities', context);
+      /*response.send(result);*/
+  });
+});
+
 app.listen(3000);
+
+/*  total fatalities request options */
+var totalFatalatiesOptions = {
+  host: 'localhost',
+  port: '8080',
+  path: '/totalfatalities/allestimates',
+  method: "GET",
+  timeout: 10000,
+  followRedirect: true,
+  maxRedirects: 10,
+  headers: {
+        'Content-Type': 'application/json'
+    }
+};
---
+++ totalfatalities.js
@@ -0,0 +1,30 @@
+var http = require("http");
+var https = require("https");
+
+exports.getTotalFatalities = function(options, onResult)
+{
+    console.log("rest::getJSON");
+
+    var port = options.port == 443 ? https : http;
+    var req = port.request(options, function(res)
+    {
+        var output = '';
+        console.log(options.host + ':' + res.statusCode);
+        res.setEncoding('utf8');
+
+        res.on('data', function (chunk) {
+            output += chunk;
+        });
+
+        res.on('end', function() {
+            var obj = JSON.parse(output);
+            onResult(res.statusCode, obj);
+        });
+    });
+
+    req.on('error', function(err) {
+        res.send('error: ' + err.message);
+    });
+
+    req.end();
+};
